eb19cc272647da75fd401d26b879374b26af45f9
hgdumadag <148425830+hgdumadag@users.noreply.github.com>
2024-05-13 14:23:55 +0800

    Update cia-agents.py

--- cia-agents.py
+++ cia-agents.py
@@ -126,16 +126,16 @@ class StreamToExpander:
         cleaned_data = re.sub(r'\x1B\[[0-9;]*[mK]', '', data)
 
         # Check if the data contains 'task' information
-        task_match_object = re.search(r'\"task\"\s*:\s*\"(.*?)\"', cleaned_data, re.IGNORECASE)
-        task_match_input = re.search(r'task\s*:\s*([^\n]*)', cleaned_data, re.IGNORECASE)
-        task_value = None
-        if task_match_object:
-            task_value = task_match_object.group(1)
-        elif task_match_input:
-            task_value = task_match_input.group(1).strip()
+        #task_match_object = re.search(r'\"task\"\s*:\s*\"(.*?)\"', cleaned_data, re.IGNORECASE)
+        #task_match_input = re.search(r'task\s*:\s*([^\n]*)', cleaned_data, re.IGNORECASE)
+        #task_value = None
+        #if task_match_object:
+        #    task_value = task_match_object.group(1)
+        #elif task_match_input:
+        #    task_value = task_match_input.group(1).strip()
 
-        if task_value:
-            st.toast(":robot_face: " + task_value)
+        #if task_value:
+        #    st.toast(":robot_face: " + task_value)
 
         # Check if the text contains the specified phrase and apply color
         if "Entering new CrewAgentExecutor chain" in cleaned_data:
